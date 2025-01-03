3b121efd4787a83d26b9f62d0b09f51d8ec981f9
Marco Marino <marino.mrc@gmail.com>
2025-01-03 17:16:17 +0100

    Added commands to change the SPI interval and the API password

--- aps_cli/config.py
+++ aps_cli/config.py
@@ -36,21 +36,39 @@ def net_show(ctx: typer.Context):
         message = typer.style(res, fg=typer.colors.RED)
         utils.print_msg(message)
 
+@app.command(name="spi-set-interval")
+def port_set(ctx: typer.Context, 
+    spi_interval: Annotated[int, typer.Argument(min=g_vars.MIN_SPI_INTERVAL, max=g_vars.MAX_SPI_INTERVAL)]):
+    """
+    Set the spi interval for the APS board
+    """
+    error, res = utils.do_post("{}/{}".format(ctx.obj.url, g_vars.API_DICT['spi-config']['url']),
+        username=ctx.obj.username, password=ctx.obj.password, data={'spi_int': spi_interval },
+        debug=ctx.obj.debug, verify=(not ctx.obj.insecure))
+
+    if not error:
+        try:
+            response = res.json()
+            if response['status'] == "OK":
+                message = typer.style("{}".format(response['message']), fg=typer.colors.GREEN, bold=True)
+            else:
+                message = typer.style("Error: {}".format(response['error']), fg=typer.colors.RED)
+            utils.print_msg(message, False, ctx.obj.debug)
+        except Exception as e:
+            message = typer.style("Error: {}".format(e), fg=typer.colors.RED)
+            utils.print_msg(message)
+    else:
+        message = typer.style(res, fg=typer.colors.RED)
+        utils.print_msg(message)
+
 @app.command(name="pw-change")
 def pw_change(ctx: typer.Context,
-            user: Annotated[str, typer.Option(help='Username')],
-            password: Annotated[str, typer.Option(prompt=True, confirmation_prompt=True, hide_input=True)]):
+            new_password: Annotated[str, typer.Option(prompt=True, confirmation_prompt=True, hide_input=True)]):
     """
-    Change the password for a user
+    Change the API password 
     """
-    utils.print_msg("Not yet implemented")
-    """if user is None:
-        user = ctx.obj.username
-    d = {}
-    d['user'] = user
-    d['pw'] = password
     error, res = utils.do_post("{}/{}".format(ctx.obj.url, g_vars.API_DICT['pw-change']['url']), username=ctx.obj.username,
-        password=ctx.obj.password, data=d, debug=ctx.obj.debug, verify=(not ctx.obj.insecure))
+        password=ctx.obj.password, data={'new_pass': new_password}, debug=ctx.obj.debug, verify=(not ctx.obj.insecure))
     if not error:
         try:
             response = res.json()
@@ -64,4 +82,4 @@ def pw_change(ctx: typer.Context,
             utils.print_msg(message)
     else:
         message = typer.style(res, fg=typer.colors.RED)
-        utils.print_msg(message)"""
+        utils.print_msg(message)
--- aps_cli/g_vars.py
+++ aps_cli/g_vars.py
@@ -4,10 +4,14 @@ from rich.console import Console
 MAX_INPUT_PORTS = 2
 MAX_OUTPUT_PORTS = 8
 MAX_MODULES = 2
+MIN_SPI_INTERVAL = 10
+MAX_SPI_INTERVAL = 5000
 
 API_DICT = {
     # Aps
     'status': {'url': 'status.json', 'method': 'get'},
+    'spi-config': {'url': 'aps_config_spi.json', 'method': 'post'},
+    'pw-change': {'url': 'aps_config_password.json', 'method': 'post'},
     # Ports
     'port-set': {'url': 'ports/set.json', 'method': 'post'},
     'port-status': {'url': 'ports/status.json', 'method': 'get'},
